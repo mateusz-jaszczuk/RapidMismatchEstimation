3d69d3847b31bfb45397e285c46d03de435ab5f2
Mateusz Jaszczuk <mateuszjaszczuk@ist2-09687.apn.wlan.private.upenn.edu>
2025-08-20 13:58:17 -0400

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# RapidMismatchEstimation
+# Rapid Mismatch Estimation
+Welcome to Rapid Mismatch Estimation GitHub Page! Below you can find attached prject code.
+
+## Robot Compatibility
+The following code was designed for Franka Emika
+
+## Using the Code
+
+
+
+### ROS Package
+
+Mention input shapes
--- activation_condition.py
+++ activation_condition.py
@@ -1,11 +1,13 @@
 def activation_condition(torque_to_check,F_ext_list):
+    ## Inputs ##
+    # torque_to_check - list of 500 tau_ext_norm
+    # F_ext_list - list or np array of 500 F_ext
     long_term_increase = np.abs((torque_to_check[-1] - torque_to_check[0])) > 0.7
     stabilization_checks = [sum(torque_to_check[-70:   -50])/20, 
                             sum(torque_to_check[-150: -130])/20, 
                             sum(torque_to_check[-230: -210])/20]
     end_stabilization = sum(torque_to_check[-10:])/10
     stabilization = all(abs(end_stabilization - checks) < 0.2 for checks in stabilization_checks)
-
     # Check forces
     F_ext = np.array(F_ext_list)
     #F_ext_mean_init = np.mean(F_ext[:20,:],axis=0)
@@ -13,3 +15,4 @@ def activation_condition(torque_to_check,F_ext_list):
     z_principal_force = (np.abs(F_ext_mean[2]) > F_ext_mean[0]) and (np.abs(F_ext_mean[2]) > F_ext_mean[0])
     side_forces = (F_ext_mean[0] < 2.5) and (F_ext_mean[1] < 2.5)
     force_check = z_principal_force and side_forces
+    return (long_term_increase and stabilization and force_check)
